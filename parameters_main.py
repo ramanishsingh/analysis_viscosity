12d19ba493fd5113106b079e29ee6f8658a9bd87
ramanishsingh <rsthakur922@gmail.com>
2021-07-06 11:56:17 -0500

    update parameters_main.py

--- parameters_main.py
+++ parameters_main.py
@@ -1,15 +1,15 @@
 from calcVisc import calcVisc
 cv = calcVisc()
-visc_dir = '../simulations/'
-visc_num = 56
-visc_skip = 2000000/5
+visc_dir = '../simulations/' #relative path of the simulations folder
+visc_num = 56                # total number of independent simulations
+visc_skip = 2000000/5        # Initial lines to be skipped in each trajectory log file i.e. eqlb period
 visc_log = 'log-output_nvt.log'
-verb = 65
+verb = 65                    
 output = {}
-visc_samples = 34
-visc_num_boots = 12
-visc_plot=True
-visc_guess = [1e-3,0.9,1e2,1e3]
+visc_samples = 34            # Number of trajectories to be included in each bootstrap
+visc_num_boots = 12          # Number of bootstraps you want
+visc_plot=True               
+visc_guess = [1e-3,0.9,1e2,1e3] # Might need to change according to the fit
 
 output = cv.calcvisc(visc_num,visc_skip,visc_dir,visc_log,output,verb,visc_samples,visc_num_boots,visc_plot,visc_guess)
 
